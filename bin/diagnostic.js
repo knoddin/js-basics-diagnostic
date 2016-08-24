4d78258d028cac082d145c35fc10a2fd098d3562
knoddin <knoddin@gmail.com>
2016-08-24 09:26:45 -0400

    2nd commit - part 2
    questions 5

--- bin/diagnostic.js
+++ bin/diagnostic.js
@@ -1,1 +1,16 @@
 'use strict';
+
+
+
+//We'll learn about require later in the course
+const ask = require('../lib/ask.js');
+
+let answer =1;
+
+while (answer !== '') {
+  answer = ask("Pick a number!");
+  if (answer>10) || (answer<1){
+   console.log("Bin needs to be between 1 and 10!");
+   break;
+  }
+}
